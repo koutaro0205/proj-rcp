5841e4f9bb24707cdd2c27e720a88176129d2ebe
koutaro0205 <koutaro130205@icloud.com>
2023-02-11 17:40:06 +0900

    KOU-113 セクションタイトル左側の太めのボーダーの角を丸める

--- frontend/src/components/atoms/Title/SectionTitle/styles.ts
+++ frontend/src/components/atoms/Title/SectionTitle/styles.ts
@@ -2,16 +2,30 @@ import { css } from '@emotion/css';
 
 import colors from '@/theme/colors';
 import fontSize from '@/theme/fontSize';
+import fontWeight from '@/theme/fontWeight';
 import space from '@/theme/space';
+
+const BORDER_WIDTH = 6;
 
 const styles = {
   sectionTitle: css({
+    position: 'relative',
     fontSize: fontSize.l,
     marginTop: space.ml,
     marginBottom: space.l,
-    borderLeft: `solid 6px ${colors.baseColor}`,
-    paddingLeft: space.xs,
-    fontWeight: 'bold',
+    paddingLeft: space.s,
+    fontWeight: fontWeight.bold,
+    ':before': {
+      content: `""`,
+      width: BORDER_WIDTH,
+      height: 36,
+      borderRadius: BORDER_WIDTH / 2,
+      background: colors.baseColor,
+      position: 'absolute',
+      left: 0,
+      top: '50%',
+      transform: 'translateY(-50%)',
+    },
   }),
 };
 
--- frontend/src/components/atoms/Title/SubTitile/styles.ts
+++ frontend/src/components/atoms/Title/SubTitile/styles.ts
@@ -4,13 +4,26 @@ import colors from '@/theme/colors';
 import fontSizes from '@/theme/fontSize';
 import space from '@/theme/space';
 
+const BORDER_WIDTH = 5;
+
 const styles = {
   container: css({
+    position: 'relative',
     fontSize: fontSizes.ml,
     marginTop: space.ml,
     marginBottom: space.l,
-    borderLeft: `solid 6px ${colors.baseColor}`,
-    paddingLeft: space.xxs,
+    paddingLeft: space.s,
+    ':before': {
+      content: `""`,
+      width: BORDER_WIDTH,
+      height: 30,
+      borderRadius: BORDER_WIDTH / 2,
+      background: colors.baseColor,
+      position: 'absolute',
+      left: 0,
+      top: '50%',
+      transform: 'translateY(-50%)',
+    },
   }),
 };
 
